9628405d2ef1e694dfd0d5bc8fc10ff06d0466a2
Karthick1672 <karthicknaren167@gmail.com>
2026-08-16 23:53:44 +0530

    Add new job listings for Zoho and IBM

--- jobnest/src/data/Jobs.js
+++ jobnest/src/data/Jobs.js
@@ -3,7 +3,7 @@ export const jobsData = [
     id: "jobnext1",
     title: "Technical Support Engineers",
     company: "Zoho Corporation",
-    companyLogo: "https://via.placeholder.com/60?text=Zoho",
+    // companyLogo: "https://via.placeholder.com/60?text=Zoho",
     location: "Chennai",
     type: "Full Time",
     experience: "0-6 Years",
@@ -38,5 +38,43 @@ export const jobsData = [
     //   "Comprehensive medical and term life insurance",
     //   "Continuous learning opportunities and product ownership"
     // ]
-  }
+  },
+  {    
+    id: "jobnext2",
+    title: "Technical Support Representative Intern",
+    company: "IBM",
+    // companyLogo: "https://via.placeholder.com/60?text=Zoho",
+    location: "Chennai",
+    type: "Full Time",
+    experience: "0 Years",
+    salary: "₹4–6 LPA",
+    category: "IT & Software",
+    // postedDate: "2026-08-16",
+    isFeatured: true,
+    applyLink: "https://careers.ibm.com/en_US/careers/JobDetail?jobId=114791&utm_source=chatgpt.com",
+    description:
+      " Your primary responsibilities include observing the following activities of IBM Consultants, and getting experience in these activities under specific circumstances:Application Monitoring (Monitoring application processes, Incident & Fault Analysis, Simple Rectification)Gain knowledge to manage Low to Medium production (Technical) support tasks and activities that require knowledge regarding application, and its functionality along with the key technology using which the application is developed Scheduling and production monitoring of applications Administration of Applications related to Services Non-code related application configuration activities",
+    // responsibilities: [
+    //   "Design, develop, and deploy scalable cloud applications using Java, C++, or Node.js.",
+    //   "Collaborate with product teams to build modular backend services and REST APIs.",
+    //   "Optimize core database queries, application architecture, and front-end performance.",
+    //   "Identify system bottlenecks and implement automated unit tests to ensure high code quality."
+    // ],
+    requirements: [
+      "Bachelor's Degree",
+      "Graduate BSc, BCA",
+      "12 Plus 3 Academic years",
+      "Pass percentage of 60 percentage throughout their academics in 10th, 12th and College",
+      "Excellent written and verbal interpersonal skills",
+      "Ability to take up challenging tasks",
+      
+    ],
+    skills: ["Analytical Skills", "Communication Skills", "Technical Skills", "Customer Support", "Web Technologies", "Communication"]
+    // benefits: [
+    //   "Competitive salary & performance incentives",
+    //   "Free daily breakfast, lunch, and snacks on campus",
+    //   "Comprehensive medical and term life insurance",
+    //   "Continuous learning opportunities and product ownership"
+    // ]
+  },
 ];
